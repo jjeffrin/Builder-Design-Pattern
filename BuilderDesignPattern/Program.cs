6484e6f991c71d5ef7c806bb11e4e62f7b71ea94
jjeffrin <jjeffrin@gmail.com>
2019-07-30 11:08:27 +0530

    Added Car class

--- BuilderDesignPattern/Program.cs
+++ BuilderDesignPattern/Program.cs
@@ -2,6 +2,18 @@
 
 namespace BuilderDesignPattern
 {
+    public class Car
+    {
+        public string EngineCapacity;
+        public string MaxSpeed;
+        public string Manufacturer;
+        public void GetCarInfo()
+        {
+            Console.WriteLine("Engine Capacity: " + EngineCapacity);
+            Console.WriteLine("Max Speed: " + MaxSpeed);
+            Console.WriteLine("Manufacturer: " + Manufacturer);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
